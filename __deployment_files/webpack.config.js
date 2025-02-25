d7a7192faddf18bfe3c5f580f5deed9cba4a1028
Puncher <puncher@gmail.com>
2025-02-25 18:31:20 +0200

    webpack english comments and config optimization

--- __deployment_files/webpack.config.js
+++ __deployment_files/webpack.config.js
@@ -1,176 +1,135 @@
-const InertEntryPlugin = require('inert-entry-webpack-plugin'); // этот плагинчик позволяет указывать в качестве точки входа также и не js-файлы
+const InertEntryPlugin = require('inert-entry-webpack-plugin');
 const path = require('path');
 const fs = require('fs');
 
-var getFiles = function(dir, buildFolder, originalDir = false) {
-    var results = {},
-        list = fs.readdirSync(dir),
-        excluded = [
-            buildFolder
-        ];
+// Recursively get all files in a directory, excluding build folder
+const getFiles = (dir, buildFolder, originalDir = null) => {
+    const results = {};
+    const baseDir = originalDir || dir;
+    const excluded = new Set([buildFolder]);
 
-    var splitDir = originalDir ? originalDir : dir;
-
-    function extend(obj, src) {
-        for (var key in src) {
-            if (src.hasOwnProperty(key)) obj[key] = src[key];
+    const processEntry = (filePath) => {
+        const stat = fs.statSync(filePath);
+        if (stat.isDirectory()) {
+            return getFiles(filePath, buildFolder, baseDir);
         }
-        return obj;
-    }
 
-    list.forEach(function(file) {
-        if (excluded.indexOf(file) === -1) {
-            file = dir + '/' + file;
-            var stat = fs.statSync(file);
-            if (stat && stat.isDirectory()) {
-                /* Recurse into a subdirectory */
-                results = extend(results, getFiles(file, buildFolder, originalDir ? originalDir : dir));
-            } else {
-                /* Is a file */
-                let split = file.split('.');
-                let base = split[0].split(splitDir);
+        const relativePath = path.relative(baseDir, filePath);
+        const parsedPath = path.parse(relativePath);
+        const outputBase = path.join(dir, buildFolder, parsedPath.dir);
 
-                let baseNew = splitDir+'/'+buildFolder+base[1];
+        return {
+            [path.join(outputBase, parsedPath.name)]: `./${filePath}`
+        };
+    };
 
-                results[baseNew] = './'+file;
-            }
+    fs.readdirSync(dir).forEach(file => {
+        if (!excluded.has(file)) {
+            const fullPath = path.join(dir, file);
+            Object.assign(results, processEntry(fullPath));
         }
     });
+
     return results;
 };
 
 /**
- *
- * @param env
- * env.NODE_ENV: среда, в которой выполняется сборка (dev - режим отладки, prod - режим запущенного сайта с минимизацией)
- * env.ENTRY_FILE: файл, передаваемый на точку входа (или несколько файлов) в формате вида `modules/popup.less` (если не передан параметр PATH_SEPARATOR), либо в формате от корня проекта `web/stat/css/modules/popup.less` (если передан параметр PATH_SEPARATOR)
- * env.DIR_PATH: рабочая директория, в которой производится сборка (собирает также поддиректории) в формате вида `web/stat/css` (данный параметр требуется, если не передан параметр PATH_SEPARATOR)
- * env.PATH_SEPARATOR: разделитель пути, используемый для получения рабочей директории и имени входного файла (если он передан, то параметр DIR_PATH перезаписывается) в формате вида `/css/`, или `css/`, или `css`
- * env.BUILD_TYPE: тип сборки (js или css)
- * env.BUILD_FOLDER: имя папки, которая будет создаваться для собранных файлов
- *
- * @returns {{output: {path: *, filename: string}, devtool: (string|boolean), entry: *, plugins: [*, *], module: {rules: *[]}}}
+ * Webpack configuration factory
+ * @param {Object} env - Environment variables
+ * @param {string} env.NODE_ENV - Environment mode ('dev' or 'prod')
+ * @param {string} env.ENTRY_FILE - Entry file(s) in format 'modules/popup.less' or full path
+ * @param {string} env.DIR_PATH - Base directory for build (e.g., 'web/stat/css')
+ * @param {string} env.PATH_SEPARATOR - Path separator for parsing entry files (e.g., '/css/')
+ * @param {string} env.BUILD_TYPE - Output type ('js' or 'css')
+ * @param {string} env.BUILD_FOLDER - Output directory name (default: 'prod')
+ * @returns {import('webpack').Configuration}
  */
 module.exports = env => {
-    let entryFiles = env.ENTRY_FILE ? env.ENTRY_FILE : '',
-        dirPath = env.DIR_PATH ? env.DIR_PATH : '',
-        pathSeparator = env.PATH_SEPARATOR ? env.PATH_SEPARATOR : '',
-        buildType = env.BUILD_TYPE ? env.BUILD_TYPE : '',
-        dirSplit = dirPath.split('/'),
-        buildFolder = env.BUILD_FOLDER ? env.BUILD_FOLDER : 'prod';
-    
-    console.log(dirPath);
-    console.log(buildFolder);
+    const {
+        ENTRY_FILE,
+        DIR_PATH = '',
+        PATH_SEPARATOR,
+        BUILD_TYPE = '',
+        NODE_ENV = 'prod',
+        BUILD_FOLDER = 'prod'
+    } = env;
 
-    if (dirSplit[0] == '')
-        dirSplit.shift();
-    if (dirSplit[dirSplit.length-1] == '')
-        dirSplit.pop();
-
-    dirPath = dirSplit.join('/');
-
+    // Normalize directory path
+    const dirPath = path.normalize(DIR_PATH).replace(/^(\.\/)|(\/+)/g, '');
     let files = {};
 
-    if (entryFiles) {
-        if (typeof entryFiles == 'string')
-            entryFiles = [entryFiles];
+    // Process entry files
+    if (ENTRY_FILE) {
+        const entryFiles = Array.isArray(ENTRY_FILE) ? ENTRY_FILE : [ENTRY_FILE];
 
-        entryFiles.forEach(function(el){
-            if (pathSeparator) {
-                let splitPathSeparator = pathSeparator.split('/');
-                if (splitPathSeparator[0] == '')
-                    splitPathSeparator.shift();
-                if (splitPathSeparator[splitPathSeparator.length-1] != '')
-                    splitPathSeparator.push('');
+        files = entryFiles.reduce((acc, filePath) => {
+            let [baseDir, ...fileParts] = PATH_SEPARATOR
+                ? filePath.split(path.normalize(PATH_SEPARATOR))
+                : [dirPath, filePath];
 
-                pathSeparator = splitPathSeparator.join('/');
+            const parsed = path.parse(fileParts.join(path.sep));
+            const outputName = path.join(baseDir, BUILD_FOLDER, parsed.dir, parsed.name);
 
-                let splitByTwo = el.split(pathSeparator);
-
-                dirPath = splitByTwo.shift()+pathSeparator.slice(0,-1);
-                el = splitByTwo.join(pathSeparator);
-            }
-
-            let fileSplit = el.split('.');
-            if (fileSplit.length > 1)
-                fileSplit.pop();
-            let fileBase = fileSplit.join('.');
-            files[dirPath+'/'+buildFolder+'/'+fileBase] = './'+dirPath+'/'+el;
-        });
+            return {
+                ...acc,
+                [outputName]: `./${path.join(baseDir, parsed.dir, parsed.base)}`
+            };
+        }, {});
     } else {
-        files = getFiles(dirPath, buildFolder);
+        files = getFiles(dirPath, BUILD_FOLDER);
     }
 
-    // console.log(files);
-
-    let filename = buildType == 'js' ? './[name].min.js' : './_build_7739fa0655a595bc5c989d8492943640.min.js';
-    let outputPath = path.resolve(__dirname, buildType == 'js' ? '../' : '../'+dirPath+'/'+buildFolder+'/');
-
-    let obj =  {
+    // Common configuration
+    const config = {
         entry: files,
         output: {
-            filename: filename,
-            path: outputPath
+            path: path.resolve(__dirname, BUILD_TYPE === 'js' ? '../' : `../${dirPath}/${BUILD_FOLDER}`),
+            filename: BUILD_TYPE === 'js' ? '[name].min.js' : 'unused_filename.min.js'
         },
+        mode: NODE_ENV === 'dev' ? 'development' : 'production',
+        devtool: NODE_ENV === 'dev' ? 'eval-source-map' : false,
         module: {
             rules: [
                 {
-                    test: /\.(less)$/,
+                    test: /\.less$/,
                     use: [
                         {
                             loader: 'file-loader',
                             options: {
-                                name: "[path][name].min.css",
-                                outputPath: (url, resourcePath, context) => {
-                                    let splitter = dirPath+'/',
-                                        outURL = url.split(splitter)[1];
-
-                                    return `../../css/${buildFolder}/${outURL}`;
-                                }
+                                name: '[path][name].min.css',
+                                outputPath: url => path.join('../../css', BUILD_FOLDER, url)
                             }
                         },
-                        {
-                            loader: 'extract-loader'
-                        },
+                        'extract-loader',
                         {
                             loader: 'css-loader',
-                            options: {
-                                url: false
-                            }
+                            options: { url: false }
                         },
                         {
-                            loader: 'less-loader' ,
-                            options: {
-                                sourceMap: true
-                            }
+                            loader: 'less-loader',
+                            options: { sourceMap: true }
                         }
                     ]
                 },
                 {
                     test: /\.js$/,
-                    exclude: /(node_modules)/,
+                    exclude: /node_modules/,
                     use: {
-                        loader: "babel-loader",
+                        loader: 'babel-loader',
                         options: {
-                            plugins: [
-                                "@babel/plugin-transform-runtime"
-                            ],
-                            presets: [
-                                "@babel/preset-env"
-                            ]
+                            plugins: ['@babel/plugin-transform-runtime'],
+                            presets: ['@babel/preset-env']
                         }
                     }
                 }
             ]
-        },
-        devtool: env.NODE_ENV === 'dev' ? 'eval-source-map' : false
+        }
     };
 
-    if (buildType != 'js') {
-        obj.plugins = [
-            new InertEntryPlugin()
-        ];
+    // Add plugins for non-JS builds
+    if (BUILD_TYPE !== 'js') {
+        config.plugins = [new InertEntryPlugin()];
     }
 
-    return obj;
+    return config;
 };
